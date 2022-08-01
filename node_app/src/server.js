dbd949411adcbea7df37a85dfd6f68411f154815
Jordan Shropshire <jordan.shropshire@gmail.com>
2022-08-01 17:25:41 -0500

    major update

---
+++ node_app/src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+var cors = require('cors')
+const PORT = process.env.PORT || 3001;
+const app = express(),
+      bodyParser = require("body-parser");
+
+
+app.use(cors())
+app.use(bodyParser.json());
+
+// place holder for the data
+const users = {"name":"boopsies"};
+
+app.get('/api/users', (req,res) => {
+  console.log('api/users called!!!!')
+  res.json(users);
+});
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  console.log('Adding user::::::::', user);
+  users.push(user);
+  res.json({"user added"});
+});
+
+app.get('/', (req, res) => {
+  console.log("received request")
+    res.json({'message':'App Works !!!!'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on the port::${PORT}`);
+});
---
+++ react_app/src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+
+function App() {
+  const axios = require('axios');
+  const[result, setResult] = useState("");
+
+  useEffect(() =>{
+    setResult("nothing");
+  },[])
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      async function getMessage() {
+        const response = await axios.get(
+          "http://localhost:3001/api/users"
+        );
+        console.log(response);
+        setResult(response.data.name);
+      }
+      getMessage();
+    } catch(err) {
+      console.log(err);
+    }
+
+
+
+    setResult("success");
+    console.log(result)
+  }
+
+
+  return (
+    <div>
+      <p>welcome to the simple app</p>
+      <form onSubmit={handleSubmit}>
+        <button variant="primary" type="submit">test</button >
+      </form>
+      
+      <p>{result}</p>
+    </div>
+  );
+}
+
+export default App;
